664afcd6b3fb15b9f34510324b1458bee9205a3b
AkajithAK <akajith919@gmail.com>
2023-03-18 21:30:25 +0530

    post api

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 const data = [
     {name:"demo"},
     {name:"demo"},
@@ -13,6 +15,15 @@ const data = [
 
 app.get('/getData',(req,res)=>{
     res.send(data)
+})
+
+app.post('/postCall',(req,res)=>{
+    console.log(req.body,"res")
+    if(req.body.email == "abc@mail.com" && req.body.password == "12345"){
+        res.send(req.body)
+    }else{
+        res.status(401).send("error")
+    }
 })
 
 
